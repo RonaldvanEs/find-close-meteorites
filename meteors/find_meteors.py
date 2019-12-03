847b711bf4015e3354d3236c7c77027d5cf0a151
Ronald van Es <ronald.van.es@cfit.nl>
2019-12-03 12:25:51 +0100

    Don't run script code when imported

--- meteors/find_meteors.py
+++ meteors/find_meteors.py
@@ -16,21 +16,23 @@ def calc_dist(lat1, lon1, lat2, lon2):
 
 def get_dist(lz):
     return lz.get('distance', math.inf)
-# 1.
-landings = requests.get("https://data.nasa.gov/resource/gh4g-9sfh.json")
-landings.status_code # should be 200
-# 2.
-meteor_data = landings.json()
 
-# 3.
-# My location Zoetermeer, 52.0691168264258 ,  4.494627242513502
-my_loc = (52.0691168264258, 4.494627242513502)
-for meteor in meteor_data:
-    if not ('reclat' in meteor and 'reclong' in meteor): continue
-    meteor['distance'] = calc_dist(float(meteor['reclat']),
-                            float(meteor['reclong']),
-                            my_loc[0],
-                            my_loc[1])
-meteor_data.sort(key=get_dist)
-# Show the firts 10 items
-print(meteor_data[0:10])
+if __name__ == '__main__':
+    # 1.
+    landings = requests.get("https://data.nasa.gov/resource/gh4g-9sfh.json")
+    landings.status_code # should be 200
+    # 2.
+    meteor_data = landings.json()
+
+    # 3.
+    # My location Zoetermeer, 52.0691168264258 ,  4.494627242513502
+    my_loc = (52.0691168264258, 4.494627242513502)
+    for meteor in meteor_data:
+        if not ('reclat' in meteor and 'reclong' in meteor): continue
+        meteor['distance'] = calc_dist(float(meteor['reclat']),
+                                float(meteor['reclong']),
+                                my_loc[0],
+                                my_loc[1])
+    meteor_data.sort(key=get_dist)
+    # Show the firts 10 items
+    print(meteor_data[0:10])
